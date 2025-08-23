48a198e6d08d1f3ad4b0388b2354903004cb9bd4
Pedro Luiz Bortot Monteiro do Rosário <60971278+PedroBMR@users.noreply.github.com>
2025-08-23 09:49:14 -0300

    feat: add VIP orders and rare order chance

--- Assets/Scripts/Gameplay/OrderData.cs
+++ Assets/Scripts/Gameplay/OrderData.cs
@@ -7,4 +7,6 @@ public class OrderData
     public WeaponSpec weaponSpec;
     public int minPayment;
     public int maxPayment;
+    // Se verdadeiro, o pedido exige assistir a um anúncio antes de ser aceito
+    public bool requiresAd;
 }
