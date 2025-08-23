ce9882a0d2f62ee16c420691d23e3fe49eec8976
Pedro Luiz Bortot Monteiro do Rosário <60971278+PedroBMR@users.noreply.github.com>
2025-08-23 10:27:06 -0300

    refactor: apply safe area without update loop

---
+++ Assets/Scripts/UI/SafeArea.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeArea : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Rect lastSafeArea;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    private void OnEnable()
+    {
+        ApplySafeArea();
+    }
+
+    private void OnRectTransformDimensionsChange()
+    {
+        ApplySafeArea();
+    }
+
+    private void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+        if (safeArea == lastSafeArea)
+        {
+            return;
+        }
+
+        lastSafeArea = safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+}
